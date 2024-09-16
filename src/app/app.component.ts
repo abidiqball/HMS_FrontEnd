35c8b9d39860c6a1ac237947adb96da92667f03d
Abid <abid.iqbal433@gmal,com>
2024-09-16 12:35:13 +0500

    Added Patient, Doctor, Department, Appointment and Billing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DoctorComponent } from "../features/doctor/doctor.component";
+import { PatientComponent } from '../features/patient/patient.component';
+import { DepartmentComponent } from '../features/department/department.component';
+import { AppointmentComponent } from '../features/appointment/appointment.component';
+import { BillingComponent } from '../features/billing/billing.component';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,NgbModule,RouterLink,RouterLinkActive, DoctorComponent, PatientComponent, DepartmentComponent, AppointmentComponent, BillingComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
